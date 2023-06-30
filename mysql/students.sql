f8b74c315d803f0ae0f99df77a42dac27e3e4ba7
thelocal69 <86276546+thelocal69@users.noreply.github.com>
2023-06-30 10:58:44 +0700

    create mysql_folder

---
+++ mysql/students.sql
@@ -0,0 +1,10 @@
+USE children;
+
+CREATE TABLE students(
+	id int NOT NULL PRIMARY KEY auto_increment,
+	full_name varchar(255) NOT NULL,
+	gender enum('MALE', 'FEMALE', 'ORTHER'),
+	age int,
+	city varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+	weight double
+);
